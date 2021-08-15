6936049ad59f30d97f564fba603051c8e2b157d0
Saddam Satria <ardhisaddam@gmail.com>
2021-08-15 11:28:52 +0700

    Update

--- src/store.js
+++ src/store.js
@@ -3,4 +3,4 @@ import {reducers} from './reducers'
 import thunk from 'redux-thunk'
 
 
-export const store = createStore(reducers,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())) 
+export const store = createStore(reducers,compose(applyMiddleware(thunk))) 
